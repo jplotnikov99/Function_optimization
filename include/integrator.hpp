e9f39356dd6d32a6dcd21e102aae19f9d60469b9
Johann Plotnikov <johann.plotnikov@online.de>
2024-07-24 11:48:01 +0200

    initial commit

--- include/integrator.hpp
+++ include/integrator.hpp
@@ -20,9 +20,9 @@ private:
 
 public:
    std::unique_ptr<Function> F;
+   Integrator(std::unique_ptr<Function> &function, const int_method m, const double x_ini, const double x_fin);
    void switch_to_grad();
    void switch_to_res();
-   Integrator(std::unique_ptr<Function> &function, const int_method m, const double x_ini, const double x_fin);
    double kronrod_61(const double l, const double r, const size_t c_i = 0);
    double adap_gauss_kronrod_15(double l, double r, const double appr, const size_t c_i = 0, const double err = 1e-10);
 
